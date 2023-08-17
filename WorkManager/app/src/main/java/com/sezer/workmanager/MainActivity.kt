62c6f0463e62d60a6a202b876c60e28decb8840e
Sezer Buğday <sezerbugday2@gmail.com>
2023-08-17 13:54:14 +0300

    sent notification

---
+++ WorkManager/app/src/main/java/com/sezer/workmanager/MainActivity.kt
@@ -0,0 +1,96 @@
+package com.sezer.workmanager
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.currentCompositionLocalContext
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.NotificationCompat
+import com.sezer.workmanager.ui.theme.WorkManagerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WorkManagerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                   AnaSayfa()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AnaSayfa()
+{
+    val context = LocalContext.current
+    Column {
+        Button(onClick = {BlidirimOlustur(context) }) {
+            Text(text = "Bildirim Gonder")
+        }
+
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Yap")
+        }
+    }
+}
+fun BlidirimOlustur(context: Context)
+{
+
+    val builder : NotificationCompat.Builder
+    val bidirimYoneticisi = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+
+    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) //app in çalıstıgı sistem oreo sürümünden yeni mi diye kontrol ediyoruz
+    {
+        val kanalId = "kanalId"
+        val kanalAd = "kanalAd"
+        val kanalTanitim = "kanalTanitim"
+        val kanalOncelik = NotificationManager.IMPORTANCE_HIGH
+
+        var kanal : NotificationChannel? = bidirimYoneticisi.getNotificationChannel(kanalId)
+        if(kanal == null)
+        {
+            kanal = NotificationChannel(kanalId,kanalAd,kanalOncelik)
+            kanal.description = kanalTanitim
+            bidirimYoneticisi.createNotificationChannel(kanal)
+
+        }
+
+        builder = NotificationCompat.Builder(context,kanalId)
+        builder.setContentTitle("Yeni Bildirim")
+            .setContentText("Bu bir bidirimdir. Bildirime tikla")
+            .setSmallIcon(R.drawable.resim)
+            .setAutoCancel(true) // bildirime tıklanınca bildirim yok olsun
+        println("Bildirim olusutur icindeyiz")
+    }
+    else
+    {
+        builder = NotificationCompat.Builder(context)
+        builder.setContentTitle("Yeni Bildirim")
+            .setContentText("Bu bir bidirimdir. Bildirime tikla")
+            .setSmallIcon(R.drawable.resim)
+            .setAutoCancel(true) // bildirime tıklanınca bildirim yok olsun
+            .priority = Notification.PRIORITY_HIGH
+    }
+    bidirimYoneticisi.notify(1,builder.build())
+}
