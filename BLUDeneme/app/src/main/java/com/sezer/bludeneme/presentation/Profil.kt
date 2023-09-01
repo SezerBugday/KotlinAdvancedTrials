909f9c64f448469819c0de0f01c58d0d640780f0
Sezer Buğday <sezerbugday2@gmail.com>
2023-09-01 15:42:52 +0300

    .

--- BLUDeneme/app/src/main/java/com/sezer/bludeneme/presentation/Profil.kt
+++
@@ -1,5 +0,0 @@
-package com.sezer.bludeneme.presentation
-
-object Profil {
-
-}
--- BLUDeneme/app/src/main/java/com/sezer/bludeneme/states/DeviceConnectionState.kt
+++
@@ -1,8 +0,0 @@
-package com.sezer.bludeneme.states
-
-import android.bluetooth.BluetoothDevice
-
-sealed class DeviceConnectionState {
-    class Connected(val device: BluetoothDevice) : DeviceConnectionState()
-    object Disconnected : DeviceConnectionState()
-}
